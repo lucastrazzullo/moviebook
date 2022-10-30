a65e162aacde0a37a116a1848f8ba46214ffc263
Luca Strazzullo <lucastrazzullo86@gmail.com>
2022-10-30 17:29:33 +0100

    Refactored movie card view

--- Moviebook/Moviebook/Classes/View/MovieCardView.swift
+++ Moviebook/Moviebook/Classes/View/MovieCardView.swift
@@ -9,149 +9,17 @@ import SwiftUI
 
 struct MovieCardView: View {
 
-    @EnvironmentObject var watchlist: Watchlist
-
     @State private var isOverviewExpanded: Bool = false
 
     let movie: Movie
 
     var body: some View {
         VStack(alignment: .leading, spacing: 30) {
-            VStack(alignment: .leading, spacing: 4) {
-                Text(movie.details.title).font(.title)
-                RatingView(rating: movie.details.rating)
-                if let releaseDate = movie.details.release {
-                    Text(releaseDate, format: .dateTime.year()).font(.caption)
-                }
-            }
-            .padding(.horizontal)
-            .padding(.vertical, 4)
-
-            Group {
-                switch watchlist.itemState(item: .movie(id: movie.id)) {
-                case .none:
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text("You haven't watched this movie.")
-                            .font(.headline)
-
-                        Text("If you add it to your watchlist, you can also add a note or you can mark it as watched and add your own vote.")
-                            .font(.subheadline)
-                            .fixedSize(horizontal: false, vertical: true)
-                            .padding(.trailing)
-
-                        Spacer()
-
-                        HStack {
-                            Button(action: { watchlist.update(state: .toWatch, for: .movie(id: movie.id)) }) {
-                                WatchlistWatermarkLabel(itemState: .none)
-                            }
-                            .buttonStyle(.borderedProminent)
-
-                            Button(action: { watchlist.update(state: .watched, for: .movie(id: movie.id)) }) {
-                                WatchlistWatermarkLabel(itemState: .watched)
-                            }
-                            .buttonStyle(.bordered)
-                        }
-                        .frame(maxWidth: .infinity)
-                    }
-
-                case .toWatch:
-                    HStack(alignment: .top, spacing: 8) {
-                        Image(systemName: "quote.opening").font(.title)
-                            .foregroundColor(.accentColor)
-
-                        VStack(alignment: .leading, spacing: 24) {
-                            VStack(alignment: .leading, spacing: 8) {
-                                Text("Suggested by Valerio.")
-                                    .font(.subheadline)
-                                    .foregroundColor(.secondary)
-
-                                Text("This movie is amazing. Great special effects.")
-                                    .fixedSize(horizontal: false, vertical: true)
-                                    .font(.body)
-                            }
-                            Button(action: { watchlist.update(state: .watched, for: .movie(id: movie.id)) }) {
-                                WatchlistIcon(itemState: .watched)
-                                Text("Mark as watched")
-                            }
-                            .buttonStyle(.borderedProminent)
-                        }
-                    }
-
-                case .watched:
-                    VStack(alignment: .leading, spacing: 24) {
-                        HStack(alignment: .top, spacing: 8) {
-                            CircularRatingView(rating: 2.5, label: "Your vote", style: .prominent)
-                            .frame(height: 150)
-
-                            Spacer()
-
-                            VStack(alignment: .trailing) {
-                                Text("You watched this movie")
-                                    .font(.headline)
-                                    .multilineTextAlignment(.trailing)
-
-                                WatermarkWatchlistButton(watchlistItem: .movie(id: movie.id))
-                            }
-                        }
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(.ultraThinMaterial.opacity(0.6))
-                        .background(Color.accentColor.opacity(0.2))
-                        .background(ZStack {
-                            AsyncImage(
-                                url: movie.details.media.backdropPreviewUrl,
-                                content: { image in
-                                    image
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                },
-                                placeholder: { Color.black }
-                            )
-                            .opacity(0.4)
-                        })
-                        .background(Color.black)
-                        .cornerRadius(12)
-
-                        HStack(alignment: .top, spacing: 8) {
-                            Image(systemName: "quote.opening").font(.title)
-                                .foregroundColor(.accentColor)
-
-                            VStack(alignment: .leading, spacing: 8) {
-                                Text("Suggested by Valerio.")
-                                    .font(.subheadline)
-                                    .foregroundColor(.secondary)
-
-                                VStack(alignment: .leading, spacing: 12) {
-                                    Text("This movie is amazing. Great special effects.")
-                                        .fixedSize(horizontal: false, vertical: true)
-                                        .font(.body)
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding()
-            .background(RoundedRectangle(cornerRadius: 8).stroke(.orange))
-
+            MovieHeaderView(details: movie.details)
+            MovieWatchlistStateView(movieId: movie.id, movieBackdropPreviewUrl: movie.details.media.backdropPreviewUrl)
             if let overview = movie.details.overview, !overview.isEmpty {
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("Overview")
-                        .font(.title2)
-
-                    Text(overview)
-                        .font(.body)
-                        .lineLimit(isOverviewExpanded ? nil : 3)
-
-                    Button(action: { isOverviewExpanded.toggle() }) {
-                        Text(isOverviewExpanded ? "Less" : "More")
-                    }
-                }
-                .padding(.horizontal)
+                MovieOverviewView(isExpanded: $isOverviewExpanded, overview: overview)
             }
-
             MovieSpecsView(movieDetails: movie.details, movieGenres: movie.genres)
         }
         .padding()
@@ -160,6 +28,165 @@ struct MovieCardView: View {
         .cornerRadius(12)
         .shadow(color: Color.black.opacity(0.12), radius: 4, y: -8)
         .animation(.default, value: isOverviewExpanded)
+    }
+}
+
+private struct MovieHeaderView: View {
+
+    let details: MovieDetails
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(details.title).font(.title)
+            RatingView(rating: details.rating)
+            if let releaseDate = details.release {
+                Text(releaseDate, format: .dateTime.year()).font(.caption)
+            }
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 4)
+    }
+}
+
+private struct MovieWatchlistStateView: View {
+
+    @EnvironmentObject var watchlist: Watchlist
+
+    let movieId: Movie.ID
+    let movieBackdropPreviewUrl: URL?
+
+    var body: some View {
+        Group {
+            switch watchlist.itemState(item: .movie(id: movieId)) {
+            case .none:
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("You haven't watched this movie.")
+                        .font(.headline)
+
+                    Text("If you add it to your watchlist, you can also add a note or you can mark it as watched and add your own vote.")
+                        .font(.subheadline)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .padding(.trailing)
+
+                    Spacer()
+
+                    HStack {
+                        Button(action: { watchlist.update(state: .toWatch, for: .movie(id: movieId)) }) {
+                            WatchlistWatermarkLabel(itemState: .none)
+                        }
+                        .buttonStyle(.borderedProminent)
+
+                        Button(action: { watchlist.update(state: .watched, for: .movie(id: movieId)) }) {
+                            WatchlistWatermarkLabel(itemState: .watched)
+                        }
+                        .buttonStyle(.bordered)
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+
+            case .toWatch:
+                HStack(alignment: .top, spacing: 8) {
+                    Image(systemName: "quote.opening").font(.title)
+                        .foregroundColor(.accentColor)
+
+                    VStack(alignment: .leading, spacing: 24) {
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Suggested by Valerio.")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+
+                            Text("This movie is amazing. Great special effects.")
+                                .fixedSize(horizontal: false, vertical: true)
+                                .font(.body)
+                        }
+                        Button(action: { watchlist.update(state: .watched, for: .movie(id: movieId)) }) {
+                            WatchlistIcon(itemState: .watched)
+                            Text("Mark as watched")
+                        }
+                        .buttonStyle(.borderedProminent)
+                    }
+                }
+
+            case .watched:
+                VStack(alignment: .leading, spacing: 24) {
+                    HStack(alignment: .top, spacing: 8) {
+                        CircularRatingView(rating: 2.5, label: "Your vote", style: .prominent)
+                        .frame(height: 150)
+
+                        Spacer()
+
+                        VStack(alignment: .trailing) {
+                            Text("You watched this movie")
+                                .font(.headline)
+                                .multilineTextAlignment(.trailing)
+
+                            WatermarkWatchlistButton(watchlistItem: .movie(id: movieId))
+                        }
+                    }
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(.ultraThinMaterial.opacity(0.6))
+                    .background(Color.accentColor.opacity(0.2))
+                    .background(ZStack {
+                        AsyncImage(
+                            url: movieBackdropPreviewUrl,
+                            content: { image in
+                                image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                            },
+                            placeholder: { Color.black }
+                        )
+                        .opacity(0.4)
+                    })
+                    .background(Color.black)
+                    .cornerRadius(12)
+
+                    HStack(alignment: .top, spacing: 8) {
+                        Image(systemName: "quote.opening").font(.title)
+                            .foregroundColor(.accentColor)
+
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Suggested by Valerio.")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+
+                            VStack(alignment: .leading, spacing: 12) {
+                                Text("This movie is amazing. Great special effects.")
+                                    .fixedSize(horizontal: false, vertical: true)
+                                    .font(.body)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding()
+        .background(RoundedRectangle(cornerRadius: 8).stroke(.orange))
+    }
+}
+
+private struct MovieOverviewView: View {
+
+    @Binding var isExpanded: Bool
+
+    let overview: String
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("Overview")
+                .font(.title2)
+
+            Text(overview)
+                .font(.body)
+                .lineLimit(isExpanded ? nil : 3)
+
+            Button(action: { isExpanded.toggle() }) {
+                Text(isExpanded ? "Less" : "More")
+            }
+        }
+        .padding(.horizontal)
     }
 }
 
