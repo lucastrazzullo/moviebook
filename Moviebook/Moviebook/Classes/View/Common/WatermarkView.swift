054f96ca2519ddff4a3ba123c296170e0a60e35a
Luca Strazzullo <lucastrazzullo86@gmail.com>
2022-11-24 18:00:05 +0100

    Updarted watermark view

--- Moviebook/Moviebook/Classes/View/Common/WatermarkView.swift
+++ Moviebook/Moviebook/Classes/View/Common/WatermarkView.swift
@@ -12,12 +12,12 @@ struct WatermarkView<Content: View>: View {
     @ViewBuilder let content: () -> Content
 
     var body: some View {
-        HStack(alignment: .center, spacing: 16) {
+        HStack(alignment: .center, spacing: 28) {
             content()
-                .frame(height: 16, alignment: .center)
+                .frame(height: 18, alignment: .center)
                 .font(.footnote)
         }
-        .padding(.horizontal, 12)
+        .padding(.horizontal, 8)
         .padding(.vertical, 8)
         .background(.black.opacity(0.8))
         .foregroundColor(.white)
