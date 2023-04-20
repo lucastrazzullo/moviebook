ac7716dcfd90541f54e67d2b4c4e32291dd03331
Luca Strazzullo <lucastrazzullo86@gmail.com>
2023-04-20 12:38:46 +0200

    Improved UI for explore view

---
+++ Moviebook/Moviebook/Classes/View/Explore/ExploreView.swift
@@ -0,0 +1,241 @@
+//
+//  ExploreView.swift
+//  Moviebook
+//
+//  Created by Luca Strazzullo on 02/09/2022.
+//
+
+import SwiftUI
+import Combine
+
+@MainActor private final class SearchContent: ObservableObject {
+
+    // MARK: Instance Properties
+
+    var title: String {
+        return NSLocalizedString("EXPLORE.SEARCH.RESULTS", comment: "") + ": " + searchKeyword
+    }
+
+    @Published var searchKeyword: String = ""
+    @Published var movies: [MovieDetails] = []
+    @Published var isLoading: Bool = false
+    @Published var error: WebServiceError? = nil
+
+    private var subscriptions: Set<AnyCancellable> = []
+
+    // MARK: Search
+
+    func start(requestManager: RequestManager) {
+        $searchKeyword
+            .debounce(for: .seconds(1), scheduler: DispatchQueue.main)
+            .sink(receiveValue: { [weak self, weak requestManager] keyword in
+                if let requestManager {
+                    self?.fetchMovies(for: keyword, requestManager: requestManager)
+                }
+            })
+            .store(in: &subscriptions)
+    }
+
+    private func fetchMovies(for keyword: String, requestManager: RequestManager) {
+        Task {
+            do {
+                error = nil
+                isLoading = true
+                movies = try await SearchWebService(requestManager: requestManager).fetchMovie(with: keyword)
+                isLoading = false
+            } catch {
+                self.isLoading = false
+                self.error = .failedToLoad(id: .init()) { [weak self] in
+                    self?.fetchMovies(for: keyword, requestManager: requestManager)
+                }
+            }
+        }
+    }
+}
+
+@MainActor private final class ExploreContent: ObservableObject {
+
+    // MARK: Types
+
+    enum Section: String, Identifiable, CaseIterable {
+        case popular
+        case upcoming
+
+        var id: String {
+            return rawValue
+        }
+
+        var name: String {
+            switch self {
+            case .upcoming:
+                return NSLocalizedString("MOVIE.UPCOMING", comment: "")
+            case .popular:
+                return NSLocalizedString("MOVIE.POPULAR", comment: "")
+            }
+        }
+    }
+
+    // MARK: Instance Properties
+
+    @Published var data: [Section: [MovieDetails]] = [:]
+    @Published var error: WebServiceError? = nil
+
+    // MARK: Instance methods
+
+    func start(requestManager: RequestManager) {
+        for section in Section.allCases {
+            fetchMovies(in: section, requestManager: requestManager)
+        }
+    }
+
+    // MARK: Data
+
+    private func fetchMovies(in section: Section, requestManager: RequestManager) {
+        Task {
+            do {
+                error = nil
+                switch section {
+                case .popular:
+                    data[section] = try await PopularWebService(requestManager: requestManager).fetch()
+                case .upcoming:
+                    data[section] = try await UpcomingWebService(requestManager: requestManager).fetch()
+                }
+            } catch {
+                self.error = .failedToLoad(id: .init()) { [weak self] in
+                    self?.fetchMovies(in: section, requestManager: requestManager)
+                }
+            }
+        }
+    }
+}
+
+struct ExploreView: View {
+
+    private struct MovieIdentifier: Identifiable {
+        let id: Movie.ID
+    }
+
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.requestManager) var requestManager
+    @EnvironmentObject var watchlist: Watchlist
+
+    @StateObject private var searchContent: SearchContent = SearchContent()
+    @StateObject private var exploreContent: ExploreContent = ExploreContent()
+
+    @State private var movieNavigationPath: NavigationPath = NavigationPath()
+    @State private var presentedMovieIdentifier: MovieIdentifier?
+
+    var body: some View {
+        NavigationView {
+            List {
+                if searchContent.isLoading || !searchContent.searchKeyword.isEmpty {
+                    SectionView(title: searchContent.title,
+                                isLoading: searchContent.isLoading,
+                                error: searchContent.error,
+                                movies: searchContent.movies,
+                                onMovieSelected: { movieIdentifier in
+                        presentedMovieIdentifier = MovieIdentifier(id: movieIdentifier)
+                    })
+                }
+
+                ForEach(ExploreContent.Section.allCases) { section in
+                    if let movies = exploreContent.data[section] {
+                        SectionView(title: section.name,
+                                    isLoading: false,
+                                    error: exploreContent.error,
+                                    movies: movies,
+                                    onMovieSelected: { movieIdentifier in
+                            presentedMovieIdentifier = MovieIdentifier(id: movieIdentifier)
+                        })
+                    }
+                }
+                .listSectionSeparator(.hidden)
+            }
+            .listStyle(.inset)
+            .navigationTitle(NSLocalizedString("EXPLORE.TITLE", comment: ""))
+            .toolbar {
+                ToolbarItem {
+                    Button(action: dismiss.callAsFunction) {
+                        Text(NSLocalizedString("NAVIGATION.ACTION.DONE", comment: ""))
+                    }
+                }
+            }
+            .searchable(
+                text: $searchContent.searchKeyword,
+                prompt: NSLocalizedString("EXPLORE.SEARCH.PROMPT", comment: "")
+            )
+            .sheet(item: $presentedMovieIdentifier) { movieIdentifier in
+                NavigationStack(path: $movieNavigationPath) {
+                    MovieView(movieId: movieIdentifier.id, navigationPath: $movieNavigationPath)
+                        .navigationDestination(for: Movie.ID.self) { movieId in
+                            MovieView(movieId: movieId, navigationPath: $movieNavigationPath)
+                        }
+                }
+            }
+            .onAppear {
+                searchContent.start(requestManager: requestManager)
+                exploreContent.start(requestManager: requestManager)
+            }
+        }
+    }
+}
+
+private struct SectionView: View {
+
+    let title: String
+    let isLoading: Bool
+    let error: WebServiceError?
+    let movies: [MovieDetails]
+    let onMovieSelected: (Movie.ID) -> Void
+
+    var body: some View {
+        Section(header: header) {
+            ForEach(movies) { movieDetails in
+                MoviePreviewView(details: movieDetails) {
+                    onMovieSelected(movieDetails.id)
+                }
+            }
+        }
+        .listSectionSeparator(.hidden)
+    }
+
+    private var header: some View {
+        HStack(spacing: 4) {
+            Text(title)
+                .font(.title3)
+                .foregroundColor(.primary)
+
+            if isLoading {
+                ProgressView()
+            }
+            if let error = error {
+                HStack {
+                    Spacer()
+                    Text("Something went wrong")
+                        .foregroundColor(.primary)
+                        .underline()
+
+                    Button(action: error.retry) {
+                        Text("Retry")
+                    }
+                    .buttonStyle(.borderless)
+                }
+            }
+        }
+    }
+}
+
+#if DEBUG
+struct ExploreView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ExploreView()
+                .environment(\.requestManager, MockRequestManager())
+                .environmentObject(Watchlist(items: [
+                    .movie(id: 954): .toWatch(reason: .none),
+                    .movie(id: 616037): .toWatch(reason: .none)
+                ]))
+        }
+    }
+}
+#endif
