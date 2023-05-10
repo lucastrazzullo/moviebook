59892b7e05af39f1e65c839f419c1b5d385d1b1b
Luca Strazzullo <lucastrazzullo86@gmail.com>
2023-05-10 10:45:25 +0200

    Cleaning up web services

--- Moviebook/Moviebook/Classes/Model/Network/Web Service/UpcomingWebService.swift
+++ Moviebook/Moviebook/Classes/Model/Network/Web Service/UpcomingWebService.swift
@@ -14,7 +14,6 @@ struct UpcomingWebService {
     func fetch() async throws -> [MovieDetails] {
         let url = try TheMovieDbDataRequestFactory.makeURL(path: "movie/upcoming")
         let data = try await requestManager.request(from: url)
-        let parsedResponse = try JSONDecoder().decode(TheMovieDbResponseWithListResults<MovieDetails>.self, from: data)
-        return parsedResponse.results
+        return try JSONDecoder().decode(TMDBResponseWithListResults<TMDBMovieDetailsResponse>.self, from: data).results.map(\.result)
     }
 }
