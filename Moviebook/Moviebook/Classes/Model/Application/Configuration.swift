56022d863ee00425644a3255f946a6e47e64d7eb
Luca Strazzullo <lucastrazzullo86@gmail.com>
2023-05-14 19:59:28 +0200

    Region not used

--- Moviebook/Moviebook/Classes/Model/Application/Configuration.swift
+++ Moviebook/Moviebook/Classes/Model/Application/Configuration.swift
@@ -9,6 +9,5 @@ import Foundation
 
 enum Configuration {
     static var language: String { Locale.current.identifier }
-    static var region: String? { Locale.current.region?.identifier }
     static var currency: String { Locale.current.currency?.identifier ?? "EUR" }
 }
