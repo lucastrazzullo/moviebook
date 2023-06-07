7873eab252450066204a109258e61807f900afcb
Luca Strazzullo <lucastrazzullo86@gmail.com>
2023-06-07 23:11:39 +0200

    Added widget target

---
+++ Moviebook/MoviebookWidget/MoviebookWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MoviebookWidgetBundle.swift
+//  MoviebookWidget
+//
+//  Created by Luca Strazzullo on 07/06/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MoviebookWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MoviebookWidget()
+    }
+}
