8c3ded4f52f2fa4a49b780abdbbf44a5f70e1c45
Luca Strazzullo <lucastrazzullo86@gmail.com>
2023-06-10 19:34:58 +0200

    Fixed search

--- Moviebook/Moviebook/Classes/View/Explore/View Model/ExploreContentViewModel.swift
+++ Moviebook/Moviebook/Classes/View/Explore/View Model/ExploreContentViewModel.swift
@@ -55,7 +55,11 @@ protocol ExploreContentDataProvider {
                     fetchNextPage = { [weak self] in self?.fetch(requestManager: requestManager, page: nextPage) }
                 }
 
-                items = items.appending(items: response.results)
+                if page == nil {
+                    items = response.results
+                } else {
+                    items = items.appending(items: response.results)
+                }
                 isLoading = false
 
             } catch {
